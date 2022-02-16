8f2eb4d4a24089dfc761731b3cfffa9626fc785a
Tokutaro Shimoda <tshimoda@c2r6p2.42quebec.com>
2022-02-16 17:45:47 -0500

    includes

--- incls/parsing.h
+++ incls/parsing.h
@@ -6,7 +6,7 @@
 /*   By: tshimoda <tshimoda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 11:18:44 by tshimoda          #+#    #+#             */
-/*   Updated: 2022/02/16 15:24:47 by tshimoda         ###   ########.fr       */
+/*   Updated: 2022/02/16 17:18:13 by tshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,8 @@ typedef struct s_parse
 	t_amb	*amb;
 }	t_parse;
 
-
+void	parse_machine(t_parse *data, char *file);
+int		ft_is_valid_file(char *file);
+void    ft_read_file(t_parse *data, char *file);
+void    ft_scan_line(t_parse *data, char *line);
 #endif
