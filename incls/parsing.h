ef7402f5368630334cefda6f8a33c3f7213a326e
Tokutaro Shimoda <tshimoda@c2r6p2.42quebec.com>
2022-02-16 15:27:04 -0500

    includes

--- incls/parsing.h
+++ incls/parsing.h
@@ -6,11 +6,25 @@
 /*   By: tshimoda <tshimoda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 11:18:44 by tshimoda          #+#    #+#             */
-/*   Updated: 2022/02/16 15:02:55 by tshimoda         ###   ########.fr       */
+/*   Updated: 2022/02/16 15:24:47 by tshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSING_H
 # define PARSING_H
+
+# include "color.h"
+
+typedef struct s_amb
+{
+	double	ratio;
+	t_rgb color;
+}	t_amb;
+
+typedef struct s_parse
+{
+	t_amb	*amb;
+}	t_parse;
+
 
 #endif
