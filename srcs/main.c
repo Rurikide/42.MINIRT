3e5915b7fc56539612140ae39dbd8bea98e8b77d
Rurikide <79031388+Rurikide@users.noreply.github.com>
2022-03-08 21:38:12 -0500

    fusion cam+test branches

--- srcs/main.c
+++ srcs/main.c
@@ -21,9 +21,7 @@ int get_hit_color(t_scene *scene, t_ray ray)
 	while (i < scene->objs->total)
 	{
 		obj = (t_shape *)scene->objs->elements[i];
-		//hit_sphere()
 		distance = obj->hit_obj(obj->shape, ray.origin, ray.direction);
-		// printf("distance = %f\n", distance);
 		if (distance > 0 && distance < closer)
 		{
 			closer = distance;
@@ -50,22 +48,16 @@ t_matrix	look_at(t_vec3 cam_ori, t_vec3 cam_dir)
 	t_vec3		random;
 	t_vec3		right;
 	t_vec3		up;
-
-	if (cam_dir.y == 1 || cam_dir.y == -1)
+	t_vec3 forward;
+	if (cam_dir.y == 1)
 	{
-		if (cam_dir.y == 1)
-		{
-			// printf("cam dir\n");
-			cam_dir.x = 1;
-			cam_dir.y = 0;
-			cam_dir.z = 0;
-		}
-		else
-		{
-			cam_dir.x = -1;
-			cam_dir.y = 0;
-			cam_dir.z = 0;
-		}
+		if (cam_dir.x == 0 && cam_dir.z == 0 )
+			cam_dir.x = 0.000001;
+	}
+	else if (cam_dir.y == -1)
+	{
+		if (cam_dir.x == 0 && cam_dir.z == 0)
+			cam_dir.x = -0.000001;
 	}
 	random = new_vector(0, 1, 0);
 	random = vec_normalize(random);
@@ -76,17 +68,23 @@ t_matrix	look_at(t_vec3 cam_ori, t_vec3 cam_dir)
 	m.d[0][0] = right.x;
 	m.d[0][1] = right.y;
 	m.d[0][2] = right.z;
+	m.d[0][3] = 0;
 	m.d[1][0] = up.x;
 	m.d[1][1] = up.y;
 	m.d[1][2] = up.z;
+	m.d[1][3] = 0;
 	m.d[2][0] = cam_dir.x;
 	m.d[2][1] = cam_dir.y;
 	m.d[2][2] = cam_dir.z;
+	m.d[2][3] = 0;
 	m.d[3][0] = cam_ori.x;
 	m.d[3][1] = cam_ori.y;
 	m.d[3][2] = cam_ori.z;
+	m.d[3][3] = 1;
 	return (m);
 }
+
+
 
 // t_vec3	add_3_vec(t_vec3 u, t_vec3 v, t_vec3 w)
 // {
@@ -107,6 +105,28 @@ t_ray		create_ray(t_vec3 origin, t_vec3 direction)
 	return (result);
 }
 
+
+/*
+gmzorz version 
+t_bool	camera_ray_init(t_scene *scene, t_ray *ray, t_real u, t_real v)
+{ 
+	//equivalent a get_direction()
+	worldspace;
+	worldspace.x = (2 * ((u + 0.5) / scene->mlx.width) - 1)
+		* scene->camera->view_range * scene->ratio;
+	worldspace.y = (1 - 2 * ((v + 0.5) / scene->mlx.height))
+		* scene->camera->view_range;
+	worldspace.z = 1;
+	// IL SET ICI le RAY ORIGIN ET DIRECTION ALORS QUE MOI JE LE FAIS DANS RAY TRACING ligne 162
+	// == ligne 175 dir = multiply_by_matrix(dir, scene-cam-matrix)
+	ray->origin = mat4_get(scene->camera->world, m_origin);
+	ray->dir = vec3_unit(mat4_mult_dir(scene->camera->world,
+				worldspace));
+	ray->max = M_INFINITY;
+	return (true);
+}
+*/
+
 t_vec3		get_direction(t_scene *scene, int x, int y)
 {
 	double fov_coeff;
@@ -114,21 +134,25 @@ t_vec3		get_direction(t_scene *scene, int x, int y)
 	double p_x;
 	double p_y;
 
+	// fov_coeff = scene->cam->view_range;
 	fov_coeff = tan((double)scene->cam->fov / 2 * M_PI / 180);
 	aspect_ratio = (double)get_mlx()->width / (double)get_mlx()->height;
 	p_x = (2 * (x + 0.5) / (double)get_mlx()->width - 1) * aspect_ratio * fov_coeff;
 	p_y = (1 - 2 * (y + 0.5) / (double)get_mlx()->height) * fov_coeff;
-	return (new_vector(p_x, p_y, 1));
+
+	// jai mis -p_x
+	return (new_vector(-p_x, p_y, 1));
 }
 
 t_ray	ray_to_pixel(t_scene *scene, int x, int y)
 {
-	t_matrix world;
 	t_ray ray;
-		world = look_at(scene->cam->origin, scene->cam->dir);
-		t_vec3 ori = multiply_by_matrix(new_vector(0, 0, 0), world);
+		// rempli ici la matrice m.
+		// scene->cam->m = look_at(scene->cam->origin, scene->cam->dir);
+		// t_vec3 ori = multiply_by_matrix(new_vector(0, 0, 0), scene->cam->m);
 		t_vec3 dir = get_direction(scene, x, y);
-		dir = multiply_by_matrix(dir, world);
+
+		dir = multiply_by_matrix(dir, scene->cam->m);
 		dir = vec_sub(dir, scene->cam->origin);
 		dir = vec_normalize(dir);
 		ray = create_ray(scene->cam->origin, dir);
@@ -143,19 +167,45 @@ void	ray_tracing(t_scene *scene)
 	int color;
 	t_ray ray;
 
-	
+	scene->cam->m = look_at(scene->cam->origin, scene->cam->dir);
 	while (y < mlx->height)
 	{
 		x = 0;
 		while (x < mlx->width)
 		{
+			// ray_to_pixel renvoie un ray origin xyz, et direction normalisé
 			ray = ray_to_pixel(scene, x, y);
+			// ray.direction = new_vector(ray.direction.x, ray.direction.y, ray.direction.z);
+			// ray.direction = rotate_dir(ray.direction, scene);
 			color = get_hit_color(scene, ray);
 			my_mlx_pixel_put(mlx, x, y, color);
 			x++;
 		}
 		y++;
 	}
+	// int sample = 0;
+	// while (sample < 1)
+	// {
+	// 	printf("-----------------------------------------\n");
+	// 	// printf("cam ori x = %f\n", scene->cam->origin.x);
+	// 	// printf("cam ori y = %f\n", scene->cam->origin.y);
+	// 	// printf("cam ori z = %f\n", scene->cam->origin.z);
+	// 	// printf("Fichier .rt cam dir x = %f\n", scene->cam->dir.x);
+	// 	// printf("Fichier .rt cam dir y = %f\n", scene->cam->dir.y);
+	// 	// printf("Fichier .rt cam dir z = %f\n", scene->cam->dir.z);
+		
+	// 	// printf("\v");
+
+	// 	// printf("ray direction x = %f\n", ray.direction.x);
+	// 	// printf("ray direction y = %f\n", ray.direction.y);
+	// 	// printf("ray direction z = %f\n", ray.direction.z);
+	// 	// printf("\v");
+	// 	// printf("ray origin x = %f\n", ray.origin.x);
+	// 	// printf("ray origin y = %f\n", ray.origin.y);
+	// 	// printf("ray origin z = %f\n", ray.origin.z);
+	// 	printf("-----------------------------------------\n");
+	// 	sample++;
+	// }
 }
 
 void	make_scene(t_scene *scene)
@@ -163,7 +213,12 @@ void	make_scene(t_scene *scene)
 	t_mlx *mlx;
 
 	mlx = get_mlx();
+	//
+	scene->cam->view_range = tan((scene->cam->fov * 0.5) * (M_PI / 180));
+	// scene->cam->m = look_at(scene->cam->origin, scene->cam->dir);
+	//
 	ray_tracing(scene);
+	//
 	hook_collection(mlx, scene);
 	mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img, 0, 0);
 	mlx_loop(mlx->mlx);
@@ -182,5 +237,5 @@ int main (int argc, char **argv)
  	scene = get_scene();
  	parse_machine(scene, argv[1]);
  	make_scene(scene);
-     return (0);
+	 return (0);
  }
--- srcs/movements/move_cam.c
+++ srcs/movements/move_cam.c
@@ -1,5 +1,93 @@
 #include "../incls/mini_rt.h"
 
+t_matrix	new_matrix(t_vec3 origin, t_vec3 forward, t_vec3 right, t_vec3 up)
+{
+	t_matrix	new;
+
+	new.d[0][0] = right.x;
+	new.d[0][1] = right.y;
+	new.d[0][2] = right.z;
+	new.d[0][3] = 0;
+	new.d[1][0] = up.x;
+	new.d[1][1] = up.y;
+	new.d[1][2] = up.z;
+	new.d[1][3] = 0;
+	new.d[2][0] = forward.x;
+	new.d[2][1] = forward.y;
+	new.d[2][2] = forward.z;
+	new.d[2][3] = 0;
+	new.d[3][0] = origin.x;
+	new.d[3][1] = origin.y;
+	new.d[3][2] = origin.z;
+	new.d[3][3] = 1;
+	return (new);
+}
+
+t_matrix	matrix_identity(void)
+{
+	t_scene *scene;
+	t_vec3	forward;
+	t_vec3	up;
+	t_vec3	right;
+	t_vec3	origin;
+
+	scene = get_scene();
+	forward = scene->cam->dir;
+
+	right = new_vector(0, 0, 1);
+	up = new_vector(0, 1, 0);
+	origin = new_vector(0, 0, 0);
+	return (new_matrix(origin, forward, right, up));
+}
+
+t_matrix	matrix_rot_y(double degrees)
+{
+	t_matrix		rotate;
+	double		radians;
+
+	radians = degrees * M_PI / 180;
+	rotate = matrix_identity();
+	rotate.d[0][0] = cos(radians);
+	rotate.d[0][2] = sin(radians);
+	rotate.d[2][0] = -sin(radians);
+	rotate.d[2][2] = cos(radians);
+	return (rotate);
+}
+
+t_matrix	matrix_rot_x(double degrees)
+{
+	t_matrix		rotate;
+	double		radians;
+
+	radians = degrees * M_PI / 180;
+	rotate = matrix_identity();
+	rotate.d[1][1] = cos(radians);
+	rotate.d[1][2] = -sin(radians);
+	rotate.d[2][1] = sin(radians);
+	rotate.d[2][2] = cos(radians);
+	return (rotate);
+}
+
+t_matrix	matrix_multi(t_matrix src, t_matrix mult)
+{
+	static t_matrix		out;
+	int			i;
+	int			j;
+
+	i = 0;
+	while (i < 4)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			out.d[i][j] = src.d[i][0] * mult.d[0][j] + src.d[i][1] * mult.d[1][j] + src.d[i][2] * mult.d[2][j] + src.d[i][3] * mult.d[3][j];
+			j++;
+		}
+		i++;
+	}
+	return (out);
+}
+
 void remake_scene(t_scene *scene, t_mlx *mlx)
 {
 	mlx_destroy_image(mlx->mlx, mlx->img);
@@ -11,19 +99,206 @@ void remake_scene(t_scene *scene, t_mlx *mlx)
 void	move_cam(int keycode, t_scene *scene)
 {
 	t_mlx *mlx;
+	t_vec3 move;
+	double angle = 3.5;
 
+	// double step = 0.2617994;
 	mlx = get_mlx();
 	if (keycode == KEY_A)
-		scene->cam->origin.x -= 1;
+	{
+		move.x = scene->cam->m.d[0][0];
+		move.y = scene->cam->m.d[0][1];
+		move.z = scene->cam->m.d[0][2];
+
+		move = vec_multiply(move, 0.5);
+		scene->cam->origin.x += move.x;
+		scene->cam->origin.y += move.y;
+		scene->cam->origin.z += move.z;
+	}
 	else if (keycode == KEY_D)
-		scene->cam->origin.x += 1;
-	else if (keycode == KEY_DOWN)
-		scene->cam->origin.y -= 1;
-	else if (keycode == KEY_UP)
-		scene->cam->origin.y += 1;
+	{
+		move.x = scene->cam->m.d[0][0];
+		move.y = scene->cam->m.d[0][1];
+		move.z = scene->cam->m.d[0][2];
+
+		move = vec_multiply(move, 0.5);
+		scene->cam->origin.x -= move.x;
+		scene->cam->origin.y -= move.y;
+		scene->cam->origin.z -= move.z;
+	}
 	else if (keycode == KEY_W)
-		scene->cam->origin.z -= 1;
+	{
+		move.x = scene->cam->m.d[2][0];
+		move.y = scene->cam->m.d[2][1];
+		move.z = scene->cam->m.d[2][2];
+
+		move = vec_multiply(move, 0.5);
+		scene->cam->origin.x += move.x;
+		scene->cam->origin.y += move.y;
+		scene->cam->origin.z += move.z;
+	}
 	else if (keycode == KEY_S)
-		scene->cam->origin.z += 1;
+	{
+		move.x = scene->cam->m.d[2][0];
+		move.y = scene->cam->m.d[2][1];
+		move.z = scene->cam->m.d[2][2];
+
+		move = vec_multiply(move, 0.5);
+		scene->cam->origin.x -= move.x;
+		scene->cam->origin.y -= move.y;
+		scene->cam->origin.z -= move.z;
+	}
+	else if (keycode == KEY_T)
+	{
+		move.x = scene->cam->m.d[1][0];
+		move.y = scene->cam->m.d[1][1];
+		move.z = scene->cam->m.d[1][2];
+
+		move = vec_multiply(move, 0.5);
+		scene->cam->origin.x += move.x;
+		scene->cam->origin.y += move.y;
+		scene->cam->origin.z += move.z;
+	}
+	else if (keycode == KEY_R)
+	{
+		move.x = scene->cam->m.d[1][0];
+		move.y = scene->cam->m.d[1][1];
+		move.z = scene->cam->m.d[1][2];
+
+		move = vec_multiply(move, 0.5);
+		scene->cam->origin.x -= move.x;
+		scene->cam->origin.y -= move.y;
+		scene->cam->origin.z -= move.z;
+	}
+	else if (keycode == KEY_UP)
+	{
+		t_scene *s;
+		t_matrix rotated;
+		t_matrix multiplied;
+		t_vec3 new;
+
+		s = get_scene();
+		printf("Amatrix [2][0] = %f\n", s->cam->m.d[2][0]);
+		printf("Amatrix [2][1] = %f\n", s->cam->m.d[2][1]);
+		printf("Amatrix [2][2] = %f\n", s->cam->m.d[2][2]);
+
+		rotated = matrix_rot_x(-angle);
+		multiplied = matrix_multi(rotated, s->cam->m);
+		s->cam->m = multiplied;
+		printf("\v");
+		printf("Bmatrix [2][0] = %f\n", s->cam->m.d[2][0]);
+		printf("Bmatrix [2][1] = %f\n", s->cam->m.d[2][1]);
+		printf("Bmatrix [2][2] = %f\n", s->cam->m.d[2][2]);
+		// new = multiply_by_matrix(s->cam->origin, s->cam->m);
+
+		// printf("new.x = %f\n", new.x);
+		// printf("new.y = %f\n", new.y);
+		// printf("new.z = %f\n", new.z);
+
+		s->cam->dir.x = s->cam->m.d[2][0];
+		s->cam->dir.y = s->cam->m.d[2][1];
+		s->cam->dir.z = s->cam->m.d[2][2];
+		printf("PITCH LOOKING DOWN\n");
+	}
+	else if (keycode == KEY_DOWN)
+	{
+		t_scene *s;
+		t_matrix rotated;
+		t_matrix multiplied;
+		t_vec3 new;
+
+		s = get_scene();
+		printf("Amatrix [2][0] = %f\n", s->cam->m.d[2][0]);
+		printf("Amatrix [2][1] = %f\n", s->cam->m.d[2][1]);
+		printf("Amatrix [2][2] = %f\n", s->cam->m.d[2][2]);
+
+		rotated = matrix_rot_x(angle);
+		multiplied = matrix_multi(rotated, s->cam->m);
+		s->cam->m = multiplied;
+		printf("\v");
+		printf("Bmatrix [2][0] = %f\n", s->cam->m.d[2][0]);
+		printf("Bmatrix [2][1] = %f\n", s->cam->m.d[2][1]);
+		printf("Bmatrix [2][2] = %f\n", s->cam->m.d[2][2]);
+		// new = multiply_by_matrix(s->cam->origin, s->cam->m);
+
+		// printf("new.x = %f\n", new.x);
+		// printf("new.y = %f\n", new.y);
+		// printf("new.z = %f\n", new.z);
+
+		s->cam->dir.x = s->cam->m.d[2][0];
+		s->cam->dir.y = s->cam->m.d[2][1];
+		s->cam->dir.z = s->cam->m.d[2][2];
+		printf("PITCH LOOKING UP\n");
+	}
+		else if (keycode == KEY_LEFT)
+	{
+		t_scene *s;
+		t_matrix rotated;
+		t_matrix multiplied;
+		t_vec3 new;
+
+		s = get_scene();
+
+
+		rotated = matrix_rot_y(-angle);
+		multiplied = matrix_multi(rotated, s->cam->m);
+		s->cam->m = multiplied;
+
+		// new = multiply_by_matrix(s->cam->origin, s->cam->m);
+
+		// printf("new.x = %f\n", new.x);
+		// printf("new.y = %f\n", new.y);
+		// printf("new.z = %f\n", new.z);
+
+		s->cam->dir.x = s->cam->m.d[2][0];
+		s->cam->dir.y = s->cam->m.d[2][1];
+		s->cam->dir.z = s->cam->m.d[2][2];
+		printf("YAW TO THE RIGHT\n");
+	}
+		else if (keycode == KEY_RIGHT)
+	{
+		t_scene *s;
+		t_matrix rotated;
+		t_matrix multiplied;
+		t_vec3 new;
+
+		s = get_scene();
+
+		rotated = matrix_rot_y(angle);
+		multiplied = matrix_multi(rotated, s->cam->m);
+		s->cam->m = multiplied;
+		// new = multiply_by_matrix(s->cam->origin, s->cam->m);
+
+		// printf("new.x = %f\n", new.x);
+		// printf("new.y = %f\n", new.y);
+		// printf("new.z = %f\n", new.z);
+
+		s->cam->dir.x = s->cam->m.d[2][0];
+		s->cam->dir.y = s->cam->m.d[2][1];
+		s->cam->dir.z = s->cam->m.d[2][2];
+		printf("YAW TO THE LEFT\n");
+	}
 	remake_scene(scene, mlx);
+}
+
+
+t_vec3	rotate_dir(t_vec3 dir, t_scene *scene)
+{
+	double	n_x;
+	double	n_y;
+	double	n_z;
+	double	a;
+
+	a = scene->cam->angle;
+	// printf("angle = %f\n", a);
+	n_x = dir.x * cos(a) - dir.z * sin(a);
+	//n_y
+	n_z = dir.x * sin(a) + dir.z * cos(a);
+	dir = vec_normalize(new_vector(n_x, dir.y, n_z));
+	a = scene->cam->angle;
+	n_x = dir.x * cos(a) - dir.y * sin(a);
+	n_y = dir.x * sin(a) + dir.y * cos(a);
+	// n_z
+	dir = vec_normalize(new_vector(n_x, n_y, dir.z));
+	return (dir);
 }
