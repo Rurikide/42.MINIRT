fd7e2d5d7e7b31eb4c41b8958e126263f7b461b1
justinebadia <79991066+justinebadia@users.noreply.github.com>
2022-03-16 09:40:53 -0400

    le cylindre

--- srcs/shapes/plane.c
+++ srcs/shapes/plane.c
@@ -34,101 +34,51 @@ t_vec3	get_cyl_norm(t_vec3 hit_p, t_shape *cyl)
 	return (norm);
 }
 
-double	calc_t(float caoc, float card, float caca, float y)
+double	calc_root(double top_roco, double toprd, double h_2, double t)
 {
-	float	t;
+	double	root;
 
-	if (y < 0)
-		t = (0 - caoc) / card;
+	if (t < 0)
+		root = -(top_roco) / toprd;
 	else
-		t = (caca - caoc) / card;
-	return (t);
+		root = (h_2 - top_roco) / toprd;
+	return (root);
 }
 
 double	hit_cylinder(void *cylinder, t_vec3 ray_origin, t_vec3 ray_direction)
 {
 	t_cy	*cyl;
-	t_vec3	a_sqrt;
-	t_vec3	right;
 	double	a;
 	double	b;
 	double	c;
 	double	disc;
+	double height = 35.0;
+	double diameter = 10;
+	double t;
+	double h_2;
+	t_vec3	top;
+	t_vec3	ro_co; // ray_origin - cylindre_origin
 
 	cyl = (t_cy *)cylinder;
-	double height = 10;
-	double diameter = 50;
-	double y;
+
+	top = vec_multiply(cyl->dir, height);
+	h_2 = vec_dot(top, top); //le produit scalaire d'un v avec lui meme est égal au carré de sa longueur
+	ro_co = vec_sub(ray_origin, cyl->origin);
 	
-	// a_sqrt = vec_sub(ray_direction, vec_multiply(cyl->dir, vec_dot(ray_direction, cyl->dir)));
-
-	// a = vec_dot(a_sqrt, a_sqrt);
-	// right = vec_sub( vec_sub (ray_origin, cyl->origin), vec_multiply(cyl->dir, vec_dot(vec_sub(ray_origin, cyl->origin), cyl->dir)));
-	// b = 2 * vec_dot(a_sqrt, right);
-	// c = vec_dot(right, right) - (cyl->rad * cyl->rad);
-	// disc = b * b - 4 * a  *c;
-	// if (disc >= 0)
-	// 	return (get_root(sqrtf(disc), b));
-	// else
-	// 	return (0);
-
-	t_vec3	top;
-
-	top = vec_sub(vec_add(cyl->origin, vec_multiply(cyl->dir, height)), cyl->origin);
-	a = vec_dot(top, top) - vec_dot(top, ray_direction) * vec_dot(top, ray_direction);
-	b = vec_dot(top, top) * vec_dot(vec_sub(ray_origin, cyl->origin), ray_direction) - vec_dot(top, vec_sub(ray_origin, cyl->origin)) * vec_dot(top, ray_direction);
-	c = vec_dot(top, top) * vec_dot(vec_sub(ray_origin, cyl->origin), vec_sub(ray_origin, cyl->origin)) - 
-		vec_dot(top, vec_sub(ray_origin, cyl->origin)) * vec_dot(top, vec_sub(ray_origin, cyl->origin)) - (diameter / 2) * (diameter / 2)
-		* vec_dot(top, top);
+	a = h_2 - vec_dot(top, ray_direction) * vec_dot(top, ray_direction);
+	b = h_2 * vec_dot(ro_co, ray_direction) - vec_dot(top, vec_sub(ray_origin, cyl->origin)) * vec_dot(top, ray_direction);
+	c = h_2 * vec_dot(ro_co, ro_co) - vec_dot(top, ro_co) * vec_dot(top, ro_co) - (diameter / 2) * (diameter / 2) * h_2;
+	
 	disc = (b * b - a * c);
-	if (disc < 0)
+	double t1 = (-b - sqrtf(disc)) / a;
+	if (disc < 0) // 0 intersection 
 		return (0);
-	y = vec_dot(top, vec_sub(ray_origin, cyl->origin)) + ((-b - sqrt((b * b - a * c))) / a) * vec_dot(top, ray_direction);
-	if (y > 0 && y < 1)
-		return (((-b - sqrt((b * b - a * c))) / a));
-	y = calc_t(vec_dot(top, vec_sub(ray_origin, cyl->origin)), vec_dot(top, ray_direction), vec_dot(top, top), y);
-	if (fabs(b + a * y) < sqrt((b * b - a * c)))
-		return (y);
+
+	t = vec_dot(top,ro_co) + (t1) * vec_dot(top, ray_direction);
+	if (t > 0 && t < h_2) // mon hit_p est devant moi et en dessous du top sinon c'est que je passe au dessus ou à coté?
+		return (t1);
+	t = calc_root(vec_dot(top, ro_co), vec_dot(top, ray_direction), h_2, t); //si je passe en dessous du top, je dois vérifier que je passe au dessus de l'origine
+	if (fabs(b + a * t) < sqrtf(disc))
+		return (t);
 	return (0);
 }
-
-// double	check_cyl_root(t_vec3 ray_origin, t_vec3 ray_direction, t_shape *cyl, double disc, double b)
-// {
-// 	double	t1;
-//   	double	t2;
-//   	double	t;
-//   	double	min;
-//   	double	max;
-// 	t_vec3 q;
-// 	t_vec3	top_cyl;
-// 	double height = 5;
-	
-// 	top_cyl = vec_add(cyl->origin, vec_multiply(cyl->origin, height));
-// 	t1 = (-b + disc) / 2;
-// 	if (t1 > 0)
-// 	{
-// 		q = vec_add(ray_origin, vec_multiply(ray_direction, t1));
-// 		if (vec_dot(cyl->dir, vec_sub(q, cyl->origin)) <= 0)
-// 			t1 = -1;
-// 		if (vec_dot(cyl->dir, vec_sub(q, top_cyl)) >= 0)
-// 			t1 = -1;
-// 	}
-// 	t2 = (-b - disc) / 2;
-// 	if (t2 > 0)
-// 	{
-// 		q = vec_add(ray_origin, vec_multiply(ray_direction, t2));
-// 		if (vec_dot(cyl->dir, vec_sub(q, cyl->origin)) <= 0)
-// 			t2 = -1;
-// 		if (vec_dot(cyl->dir, vec_sub(q, top_cyl)) >= 0)
-// 			t2 = -1;
-// 	}
-// 	if (t1 < 0 && t2 < 0)
-// 		return (0.0);
-// 	min = fmin(t1, t2);
-// 	max = fmax(t1, t2);
-// 	if (min >= 0)
-// 		t = min;
-// 	else
-// 		t = max;
-// 	return (t);
-// }
