476366a65e5246cc9f0064daae498fbdaebe4e1e
justinebadia <79991066+justinebadia@users.noreply.github.com>
2022-03-16 17:16:26 -0400

    cylinder working ?????

--- srcs/shapes/plane.c
+++ srcs/shapes/plane.c
@@ -34,6 +34,16 @@ t_vec3	get_cyl_norm(t_vec3 hit_p, t_shape *cyl)
 	return (norm);
 }
 
+t_vec3	vec_divide(t_vec3 u, double scalar)
+{
+	t_vec3	resultante;
+
+	resultante.x = u.x / scalar;
+	resultante.y = u.y / scalar;
+	resultante.z = u.z / scalar;
+	return (resultante);
+}
+
 double	calc_root(double top_roco, double toprd, double h_2, double t)
 {
 	double	root;
@@ -52,16 +62,17 @@ double	hit_cylinder(void *cylinder, t_vec3 ray_origin, t_vec3 ray_direction)
 	double	b;
 	double	c;
 	double	disc;
-	double height = 35.0;
-	double diameter = 10;
-	double t;
+	double height = 80.0;
+	double diameter = 20;
+	double t = 0;
 	double h_2;
 	t_vec3	top;
 	t_vec3	ro_co; // ray_origin - cylindre_origin
 
 	cyl = (t_cy *)cylinder;
 
-	top = vec_multiply(cyl->dir, height);
+
+	top = vec_multiply(vec_normalize(vec_multiply(cyl->dir, height)), height);
 	h_2 = vec_dot(top, top); //le produit scalaire d'un v avec lui meme est égal au carré de sa longueur
 	ro_co = vec_sub(ray_origin, cyl->origin);
 	
@@ -74,11 +85,25 @@ double	hit_cylinder(void *cylinder, t_vec3 ray_origin, t_vec3 ray_direction)
 	if (disc < 0) // 0 intersection 
 		return (0);
 
+	/*mon hit_p est devant moi et sur ou en dessous du top sinon c'est que je passe au dessus ou à coté? 
+	Si mon t est négatif c'est que je suis en dessous du point d'origine
+	Si mon t est supérieur à h_2, c'est que je suis au dessus top*/ 
 	t = vec_dot(top,ro_co) + (t1) * vec_dot(top, ray_direction);
-	if (t > 0 && t < h_2) // mon hit_p est devant moi et en dessous du top sinon c'est que je passe au dessus ou à coté?
+	if (t > 0 && t < h_2) 
 		return (t1);
-	t = calc_root(vec_dot(top, ro_co), vec_dot(top, ray_direction), h_2, t); //si je passe en dessous du top, je dois vérifier que je passe au dessus de l'origine
+	t = calc_root(vec_dot(top, ro_co), vec_dot(top, ray_direction), h_2, t); //si je passe en dessous du top, je dois vérifier que je passe au dessus du bas du cylindre
 	if (fabs(b + a * t) < sqrtf(disc))
 		return (t);
 	return (0);
+
+	// t_vec3 a_sqrt = vec_sub(ray_direction, vec_multiply(cyl->dir, vec_dot(ray_direction, cyl->dir)));
+ 	// a = vec_dot(a_sqrt, a_sqrt);
+ 	// t_vec3 right = vec_sub( vec_sub (ray_origin, cyl->origin), vec_multiply(cyl->dir, vec_dot(vec_sub(ray_origin, cyl->origin), cyl->dir)));
+ 	// b = 2 * vec_dot(a_sqrt, right);
+ 	// c = vec_dot(right, right) - (cyl->rad * cyl->rad);
+ 	// disc = b * b - 4 * a  *c;
+ 	// if (disc >= 0)
+ 	// 	return (get_root(sqrtf(disc), b));
+ 	// else
+ 	// 	return (0);
 }
