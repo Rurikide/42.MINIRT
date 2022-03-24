102cfca51e5e6e6a5a443a1306dcdd46d005a06c
Tokutaro Shimoda <tshimoda@c2r3p7.42quebec.com>
2022-03-24 14:33:53 -0400

    fix segfault

--- srcs/vector_array/vector_utils.c
+++ srcs/vector_array/vector_utils.c
@@ -6,7 +6,7 @@
 /*   By: tshimoda <tshimoda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/29 12:04:58 by tshimoda          #+#    #+#             */
-/*   Updated: 2022/03/18 16:58:38 by tshimoda         ###   ########.fr       */
+/*   Updated: 2022/03/24 14:24:18 by tshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	vector_free_elements(t_vector *v, void *del(void *))
 	size_t	i;
 
 	i = 0;
-	while (v->elements[i])
+	while (i < v->total)
 	{
 		if (del != NULL)
 		{
